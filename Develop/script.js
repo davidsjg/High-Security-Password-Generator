a18bbfba2c7e6e05c6a477f06b0a48db542c7264
Joey Davidson <j.davidson18@gmail.com>
2021-01-22 22:12:24 -0700

    user entry: password length completed

--- Develop/script.js
+++ Develop/script.js
@@ -13,3 +13,38 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//propmt user if lowercase letters are acceptable
+var options = ["Y", "N"];
+var PWL = 0;
+
+
+var LowerBoolean = function () {
+  var PWL = window.prompt("Enter password length (must be between 8-128): ");
+    if ((PWL < 8) || (PWL > 128)) {
+      if (!window.alert("Password must be between 8-128 characters."))
+      LowerBoolean()
+    } 
+  console.log(PWL)
+
+
+}
+
+
+LowerBoolean()
+
+
+
+
+
+// var playGame = function() {
+//   // Ask user for their choice
+//   var PWLength = window.prompt("Enter R, P, or S:");
+
+//   // If user pressed Cancel, immediately end function
+//   if (!userChoice) {
+//     return;
+//   }
+
+//generate 3 arrays.  
+//1. 
